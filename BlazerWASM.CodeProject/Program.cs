87092bba13e2a3c8acd255071ea8f1a7591f3bcc
Israel Davis <idavi72@wgu.edu>
2024-06-07 12:43:49 -0400

    Added a modal funcitonlity to the blazer web app. Will see what else I can do with it.

--- BlazerWASM.CodeProject/Program.cs
+++ BlazerWASM.CodeProject/Program.cs
@@ -1,4 +1,5 @@
 using BlazerWASM.CodeProject;
+using Blazored.Modal;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredModal();
 
 await builder.Build().RunAsync();
